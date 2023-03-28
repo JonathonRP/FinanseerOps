6d2b321fa15122407fb3049f9fa5668e34073cdf
JonathonRP <jreesep@gmail.com>
2023-03-28 13:57:03 +0000

    further clean up

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,11 +1,10 @@
 // import * as Sentry from '@sentry/svelte';
-import type { HandleClientError } from '@sveltejs/kit';
 
 // Sentry.init({
 /* ... */
 // });
 
-export const handleError = (async ({ error, event }) => {
+export async function handleError({ error, event }) {
 	const errorId = crypto.randomUUID();
 	// example integration with https://sentry.io/
 	// Sentry.captureException(error, { event, errorId });
@@ -16,6 +15,5 @@ export const handleError = (async ({ error, event }) => {
 
 	return {
 		message: (error as Error).message ?? 'Whoops!',
-		code: errorId ?? 'UNKNOWN',
 	};
-}) satisfies HandleClientError;
+}
