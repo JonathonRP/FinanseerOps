72e7e86c0525a2644d3abfecc44e4bde8dc24984
= <=>
2023-04-24 01:04:52 +0000

    fix vercel url

--- src/routes/(anonymous)/dailyCashFlowSummaryReport/+server.ts
+++ src/routes/(anonymous)/dailyCashFlowSummaryReport/+server.ts
@@ -3,8 +3,7 @@ import { logger } from '$lib/server/logger';
 import { json } from '@sveltejs/kit';
 import { appRouter } from '$lib/server/api';
 import { createContext } from '$lib/server/api/trpc';
-import { BUXFER_EMAIL as SERVER_USER, EMAIL_FROM, SERVER_PASS } from '$env/static/private';
-import { VERCEL_URL } from '$env/dynamic/private';
+import { BUXFER_EMAIL as SERVER_USER, EMAIL_FROM, SERVER_PASS, VERCEL_URL } from '$env/static/private';
 import type { RequestHandler } from './$types';
 
 // TODO - evaluate Upstash and Vercel cronjobs alternatives.
